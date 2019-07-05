fc5802426cf5f0fb5abd424371aa092e65a4a863
M3dic <ivo_radev2@abv.bg>
2019-07-06 00:45:54 +0300

    ready declaration stars

--- CorePractise/CorePractise/Program.cs
+++ CorePractise/CorePractise/Program.cs
@@ -7,19 +7,17 @@ namespace CorePractise
     {
         static void Main(string[] args)
         {
-            Stars[] stars = new Stars[2000];
-            Stars s = new Stars("IvoRadev", 500.5848);
-            Stars s1 = new Stars("IvoIvanovRadev14July2001.", -500);
-            s.Print();
-            s1.Print();
+           
         }
     }
     public class Stars
     {
         private string name { get; set; }
         public string Name { get { return name; } set { if (value.Length > 20) { this.name = value.Remove(20); } else this.name = value; } }
+
         private double distance { get; set; }
         public double Distance { get { return this.distance; } set { if (value < 0) { this.distance = 0; } else this.distance = value; } }
+
         public enum classification { hipergigants, ultragigants, freshgigants, gigants, subgigants, dwarfs, subdwarfs, reddwarfs, browndwarfs };
         private string Classific { get; set; }
         private string NUmberClassification
@@ -38,21 +36,22 @@ namespace CorePractise
                     case "7": this.Classific = classification.subdwarfs.ToString(); break;
                     case "8": this.Classific = classification.reddwarfs.ToString(); break;
                     case "9": this.Classific = classification.browndwarfs.ToString(); break;
-                    default:
-                        break;
+                    default:  this.Classific = "Nope"; break;
                 }
             }
         }
 
-        public Stars(string name, double dis)
+        private double mass { get; set; }
+        public double Mass { get { return mass; } set { if (value < 0) this.mass = 0; else this.mass = value; } }
+
+        private string constellation { get; set; }
+        public string Constellation { get { return constellation; } set { if (value.Length > 30) this.constellation = value.Remove(30); else this.constellation = value; } }
+
+        public Stars()
         {
-            this.Name = name;
-            this.Distance = dis;
+           
         }
-        public void Print()
-        {
-            Console.WriteLine(this.name + " " + this.distance);
-        }
+       
 
     }
 }
