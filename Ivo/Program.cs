efba1e020fa10f653652978c5b0c4e3b7c46bafb
M3dic <ivo_radev2@abv.bg>
2019-07-05 22:07:41 +0300

    some commit

---
+++ Ivo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    internal class Cities
+    {
+        public string Ss { get; set; }
+        public int s { get; private set; }
+    }
+}
